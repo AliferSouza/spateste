c80a325ae0b5896e0006c6dfb23283c9b37b9dbb
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-03-27 22:17:51 -0300

    Update data.js

--- src/dados/data.js
+++ src/dados/data.js
@@ -2,7 +2,7 @@
 const cardDateColaboradores = [    
     {id:"vivi",img :"./src/img/vivi.jpg",vivi:'https://www.instagram.com/viviane.snt_/'},
     {id:"alifer", img:"./src/img/alifer.jpg",alifer:'https://www.instagram.com/alifer_oliveira_souza/'},
-    {id:"ester",img:"./src/img/ester.jpg",ester:'https://www.instagram.com/grazileite.integrar/'},  
+    {id:"ester",img:"./src/img/Ester.jpg",ester:'https://www.instagram.com/grazileite.integrar/'},  
     {id:"lest",img:"./src/img/lest.jpg",lest:'https://www.instagram.com/_drac_lb/'}
   
  
