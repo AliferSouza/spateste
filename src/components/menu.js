b0f468ce64312664eef8b63939cfe89bafa285da
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-06-01 00:22:30 -0300

    Add files via upload

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,18 +1,23 @@
 export default async function Menu(props) {
 
 
+
+   const state = () =>{
+
+   }
   
   const html = () => {
     return `   
     <nav>
-    <label class="logo"> <img data-href="/"  src="./src/img/logo.png" height="75vh"></label>
+    <label class="logo"> <img data-href="/#/home/"  src="./src/img/logo.png" height="75vh"></label>
     <input type="checkbox" id="check">
     <label for="check" class="checkbtn">
       &#x2630
     </label>
   
     <ul>
-      <li><a class="active" data-href="/">Início</a></li> 
+      <li><a class="active" data-href="/#/">Início</a></li> 
+      <li><a class="active" data-href="/#/teste">Bate-papo</a></li> 
     </ul>
   </nav>
 
@@ -21,6 +26,7 @@ export default async function Menu(props) {
   };
 
   return {
-    html
+    html,
+    state
   }
 }
---
+++ src/lib/src/use/app/Router.js
@@ -0,0 +1,156 @@
+const ROOT = document.querySelector("#app");
+
+
+function customTags(sortedOut, components) {
+  const state = []
+  try {
+    const tagNames = sortedOut.match(/<(comp-[a-z]+)/g);
+    const div = document.createElement("div");
+    div.innerHTML = sortedOut;
+
+    if (!tagNames) {
+      ROOT.innerHTML = sortedOut;
+      return;
+    }
+
+    tagNames.forEach(async (elem, i) => {
+      const compAlgo = elem.match(/(comp-[a-z]+)/g)[0];
+      const elementDom = div.querySelector(compAlgo);
+      if (!elementDom) {
+        console.log(`Não foi possível encontrar a tag ${compAlgo}`);
+        return;
+      }
+
+     const attributes = { ...elementDom.dataset };
+
+     const newTag = document.createElement(`${compAlgo}-${i}`);
+      elementDom.parentNode.replaceChild(newTag, elementDom)
+         
+      Object.entries(attributes).forEach(([key, value]) => {
+        newTag.setAttribute(key, value);
+      });  
+    
+
+      const dataApp = {
+        referencia: i,
+        nameTag: newTag.tagName.toLowerCase(),
+        attributes: attributes,
+        parameter: Object.fromEntries(new URLSearchParams(location.href.split("?")[1]).entries()),
+        pagina: location.hash.replace("#", "").match(/^\/(\w+)(\/)?/)
+      }; 
+          
+      const html = await components[compAlgo](dataApp)     
+      newTag.innerHTML = html.html()  
+      if (typeof html.state === "function") { 
+        html.state()
+        state.push(html)  
+      } 
+   
+             
+    });
+    ROOT.innerHTML = "";
+    ROOT.appendChild(div);
+    
+
+   
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function stateURL(Pages) {
+  let URL;
+  const dataUrl = location.hash.replace("#", "") || location.pathname;
+  if (dataUrl === "/#" || dataUrl === "/#/" || dataUrl === "/") {
+    URL = "/";
+  } else {
+    const match = dataUrl.match(/^\/(\w+)(\/)?/);
+    if (match && match[1]) {
+      URL = match[1];
+    }
+  }
+  
+
+  let currentPage =
+    URL === "/" || URL === ""
+      ? Object.keys(Pages)[0]
+      : Pages[URL]
+      ? URL
+      : Pages["erro"]
+      ? Pages["erro"]()
+      : erroPage(Pages);
+
+  return currentPage;
+}
+
+async function render(Pages, selectedPage, components) {
+  let sortedOut = await Pages[selectedPage]();
+  let state;
+  let html;
+
+  if (typeof sortedOut.html === "function") {
+    html = sortedOut.html();
+  } else {
+    html = sortedOut;
+  }
+
+  if (typeof sortedOut.state === "function") {
+    state = sortedOut.state;
+  }
+
+  if (components === undefined) {
+    ROOT.innerHTML = await html;
+    if (typeof sortedOut.state === "function") {
+      sortedOut.state();
+    }
+
+  } else {
+    customTags(html, components);
+  }
+}
+
+function debounce(fn, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+function erroPage(Pages) {
+  ROOT.innerHTML = `
+    <div class="erroPages">
+      ${Object.keys(Pages)
+        .map(
+          (page, index) => `
+        <a id="${index}" style="color: #fff" data-href="/#/${page}/">${page.toUpperCase()}</a>
+      `
+        )
+        .join("")}
+    </div>
+  `;
+}
+
+export default function Router(Pages, components) {
+  function routerState() {
+    const selectedPage = stateURL(Pages);
+    if (!selectedPage) {
+    } else {
+      render(Pages, selectedPage, components);
+    }
+  }
+  function handleClick(e) {
+    if (e.target.matches("[data-href]")) {
+      e.preventDefault();
+      const href = e.target.dataset.href;
+      history.pushState(null, null, href);
+      routerState();
+    }
+  }
+  window.addEventListener("popstate", routerState);
+  document.body.addEventListener("click", debounce(handleClick, 200));
+  routerState();
+}
+
