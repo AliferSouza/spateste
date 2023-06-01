3ae6d3cf0ccc41112d1ead441aaeb33841c5d688
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-06-01 13:20:21 -0300

    Update whatsapp.js

--- src/components/whatsapp.js
+++ src/components/whatsapp.js
@@ -3,7 +3,7 @@ export default async function Whatsapp(props) {
 const html = () => {
   return`
   <a href="https://api.whatsapp.com/send?phone=553171685900&text=OI" target="_blank" class="whatsapp-button">
- <img src="./src/img/whatsapp.png" width="40">
+ <img src="./src/img/Whatsapp.png" width="40">
 </a>
 `
 }
