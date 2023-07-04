dff2e0d326e514d41aabb6c0d9a382cdb4fba6e3
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-07-03 22:25:04 -0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,15 +5,7 @@ export default async function Home() {
   const html = () => {
     return `
     <comp-menu> </comp-menu>
-    <comp-slider data-class="container-slider" id="1"></comp-slider>
-    <comp-colaboradores> </comp-colaboradores>
-    <h3>Promoções e pacotes</h3>
-    <comp-cardpodutos></comp-cardpodutos>
-    <h3>Terapias e massagens</h3>
-    <comp-cardpodutos></comp-cardpodutos>
-    <h3>Spa -Vida Sants</h3>
-    <comp-cardpodutos></comp-cardpodutos>
-    <comp-newsletter data-class="newsletter"> </comp-newsletter>
+
 
   
     `
